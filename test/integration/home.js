0310e1c6639f80a57578d9bb45d9b73fd30042b0
Zach Latta <zach@zachlatta.com>
2014-02-12 02:18:02 -0800

    Call to action button links to /apply.

--- test/integration/home.js
+++ test/integration/home.js
@@ -1,11 +1,9 @@
 'use strict';
 
-beforeEach(function() {
-  browser.driver.manage().window().setSize(800, 600);
-});
-
 var Homepage = function() {
   this.navbarName = element(by.className('title'));
+  this.callToAction = element(by.className('call-to-action'));
+  this.callToActionButton = element(by.linkText('Start a Hack Club'));
 
   this.get = function() {
     browser.get('/');
@@ -24,5 +22,22 @@ describe('homepage', function() {
     homepage.get();
 
     expect(homepage.navbarName.getText()).toEqual('hackEDU');
+  });
+
+  describe('call to action', function() {
+    it('should exist', function() {
+      var homepage = new Homepage();
+      homepage.get();
+
+      expect(homepage.callToAction.getText()).toContain('Start a Hack Club');
+    });
+
+    it('should have a link to the apply page', function() {
+      var homepage = new Homepage();
+      homepage.get()
+
+      homepage.callToActionButton.click();
+      expect(browser.getCurrentUrl()).toContain('/apply');
+    });
   });
 });
