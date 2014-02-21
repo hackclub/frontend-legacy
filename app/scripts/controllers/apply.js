74a965910e22937a5c2ce92f6afb77a9e2233999
Zach Latta <zach@zachlatta.com>
2014-02-21 07:46:10 -0800

    Post application as a seperate object.

--- app/scripts/controllers/apply.js
+++ app/scripts/controllers/apply.js
@@ -5,12 +5,17 @@ var apiBase;
 angular.module('hackeduApp')
   .controller('ApplyCtrl', function($scope, $http) {
     $scope.user = {};
+    $scope.application = {};
+    $scope.registrationForm = {
+      user: $scope.user,
+      application: $scope.application
+    }
 
     $scope.createUser = function () {
       $http({
         method: 'POST',
         url: apiBase + '/apply',
-        data: $scope.user
+        data: registrationForm
       });
     };
   });
