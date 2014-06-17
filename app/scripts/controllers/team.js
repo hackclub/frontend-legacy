8256443c8a1729a89866c7ac71d52c97f8c30439
Zach Latta <zach@zachlatta.com>
2014-06-16 16:02:08 -1000

    Alphabetical team member ordering.

--- app/scripts/controllers/team.js
+++ app/scripts/controllers/team.js
@@ -4,12 +4,6 @@ angular.module('hackeduApp')
   .controller('TeamCtrl', function ($scope) {
     $scope.team = [
       {
-        name: 'Zach Latta',
-        title: 'Founder',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt vel velit non vulputate. Aliquam.',
-        icon: 'http://4.bp.blogspot.com/-zsbDeAUd8aY/US7F0ta5d9I/AAAAAAAAEKY/UL2AAhHj6J8/s1600/facebook-default-no-profile-pic.jpg'
-      },
-      {
         name: 'Ankit Ranjan',
         title: 'Developer',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt vel velit non vulputate. Aliquam.',
@@ -20,6 +14,12 @@ angular.module('hackeduApp')
         title: 'Developer',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt vel velit non vulputate. Aliquam.',
         icon: 'http://4.bp.blogspot.com/-zsbDeAUd8aY/US7F0ta5d9I/AAAAAAAAEKY/UL2AAhHj6J8/s1600/facebook-default-no-profile-pic.jpg'
-      }
+      },
+      {
+        name: 'Zach Latta',
+        title: 'Founder',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt vel velit non vulputate. Aliquam.',
+        icon: 'http://4.bp.blogspot.com/-zsbDeAUd8aY/US7F0ta5d9I/AAAAAAAAEKY/UL2AAhHj6J8/s1600/facebook-default-no-profile-pic.jpg'
+      },
     ];
   });
