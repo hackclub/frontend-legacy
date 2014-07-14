27de7985e56edd6576f267716a192bf3c4188b34
Zach Latta <zach@zachlatta.com>
2014-07-14 15:19:23 -0700

    Add logout button.

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('hackeduApp')
-  .controller('NavCtrl', function($scope, $location, Auth) {
+  .controller('NavCtrl', function($scope, $location, Auth, Session) {
     $scope.isActive = function (viewLocation) {
       return viewLocation === $location.path();
     };
 
     $scope.isAuthenticated = Auth.isAuthenticated;
+
+    $scope.logout = function () {
+      Session.destroy();
+      // TODO: Go to homepage
+      $location.path('/login');
+    };
   });
